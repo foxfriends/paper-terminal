fdf1ce27598e1610f31502de8d2b18768efada02
Cameron Eldridge <cameldridge@gmail.com>
2019-11-23 16:33:57 -0500

    Get paper to respect the syncat_active_style env var

--- src/dirs.rs
+++ src/dirs.rs
@@ -1,5 +1,6 @@
 use directories::ProjectDirs;
 use std::path::PathBuf;
+use std::env;
 
 fn syncat_directories() -> ProjectDirs {
     ProjectDirs::from("com", "cameldridge", "syncat").unwrap()
@@ -7,4 +8,9 @@ fn syncat_directories() -> ProjectDirs {
 
 pub fn syncat_config() -> PathBuf {
     syncat_directories().config_dir().to_owned()
+}
+
+pub fn active_color() -> PathBuf {
+    let active_color = env::var("syncat_active_style").unwrap_or("active".to_string());
+    syncat_config().join("style").join(active_color)
 }
